a5a895e285645a13e1ddc3b1a9799523d1b34c36
Iam-Bread <brendanscottmiller98@gmail.com>
2021-01-16 19:57:30 +0000

    i2c sending commands, dht sensor working

--- ArduinoUno_Code/ArduinoUno_Code.ino
+++ ArduinoUno_Code/ArduinoUno_Code.ino
@@ -59,27 +59,53 @@
 
 #include <Wire.h>
 
-void setup()
-{
-  Wire.begin(4);                // join i2c bus with address #4
-  Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
-}
+//void setup()
+//{
+//  Wire.begin(4);                // join i2c bus with address #4
+//  Wire.onRequest(receiveEvent); // register event
+//  Serial.begin(9600);           // start serial for output
+//}
+//
+//void loop()
+//{
+//  delay(100);
+//}
+//
+//// function that executes whenever data is received from master
+//// this function is registered as an event, see setup()
+//void receiveEvent()
+//{
+//  while(1 < Wire.available()) // loop through all but the last
+//  {
+//    char c = Wire.read(); // receive byte as a character
+//    Serial.print(c);         // print the character
+//  }
+//
+//}
+//
 
-void loop()
-{
-  delay(100);
-}
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
-void receiveEvent(int howMany)
-{
-  while(1 < Wire.available()) // loop through all but the last
-  {
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
-  }
-  int x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
+// Include the required Wire library for I2C<br>#include <Wire.h>
+
+int x = 0;
+void setup() {
+  // Define the LED pin as Output
+
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9); 
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);   
+}
+void receiveEvent(int bytes) {
+  //while(1 < Wire.available()){
+     x = Wire.read();    // read one character from the I2C
+     Serial.print(x);
+//  }
+}
+void loop() {
+  //If value received is 0 blink LED for 200 ms
+  delay(500);
+  //Serial.print(x);  
+
 }
--- I2C_ESP_MASTER/I2C_ESP_MASTER.ino
+++ I2C_ESP_MASTER/I2C_ESP_MASTER.ino
@@ -1,60 +1,43 @@
 #include <Wire.h>
 //
-////12c pins
-//#define I2C_SDA 21
-//#define I2C_SCL 22
-//#define arduinoUno 5 //arduino address
-//
-//
-//
-//
-//unsigned long delayTime;
+//#define arduinoUno 4
 //
 //void setup() {
 //  Serial.begin(115200);
 //  //start i2c bus
-//  Wire.begin(I2C_SDA, I2C_SCL, 100000);
-//
+//  Wire.begin(); //i2c start with default pins 21 and 22
 //  bool status;
 //
-//  // check if arduino is connected
+//  //check if arduino is connected
 //  status = Wire.begin(arduinoUno);  
 //  if (!status) {
 //    Serial.println("Could not find Arduino Uno, check wiring!");
 //    while (1);
 //  }
-//
 //  Serial.println("-- Default Test --");
-//  delayTime = 1000;
-//
-//  Serial.println();
 //}
 //
-//void loop() { 
-//  char c = 53;
-//  Wire.beginTransmission(5);
-//  Wire.write(c);
-//  Wire.endTransmission();
-//  Serial.println(c);
-//  delay(delayTime);
+//byte x = 0;
+//
+//void loop()
+//{
+//  Wire.beginTransmission(4); // transmit to device #4
+//  Wire.write(x);              // sends one byte  
+//  Wire.endTransmission();    // stop transmitting
+// Serial.println(x);
+//  x++;
+//  delay(500);
 //}
-
-
-
-void setup()
-{
-  Wire.begin(); // join i2c bus (address optional for master)
+int x = 0;
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin(); 
 }
-
-byte x = 0;
-
-void loop()
-{
-  Wire.beginTransmission(4); // transmit to device #4
-  Wire.write("x is ");        // sends five bytes
-  Wire.write(x);              // sends one byte  
+void loop() {
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(x);              // sends x 
   Wire.endTransmission();    // stop transmitting
-
-  x++;
+  x++; // Increment x
+  if (x > 5) x = 0; // `reset x once it gets 6
   delay(500);
 }
